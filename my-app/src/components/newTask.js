f1da8d427d5dccbc8084118f3feca96f5159a549
skrudg <60151747+skrudg@users.noreply.github.com>
2020-04-04 19:31:12 +0600

    Закончил работу над вводом новых задач

--- my-app/src/components/newTask.js
+++ my-app/src/components/newTask.js
@@ -1,10 +1,26 @@
-import React, {Component} from 'react';
 
-class newTask extends Component
+function Click()
 {
-    render(){
-        return <input value={this.props.name}/>;
+    let inputValue = document.getElementById('Input').value;
+    if(inputValue === "")
+    {
+      alert("Введите задание...");
     }
+    else
+    {
+      let resultBox = document.getElementById('mainBox__resultBox');
+
+      let newItemBox = document.createElement("div");
+      newItemBox.setAttribute('class', 'resultBox__newitemBox');
+
+      let newItem = document.createElement('input');
+      newItem.setAttribute("value", inputValue);
+      newItem.setAttribute('class', 'resultBox__newItem');
+
+      newItemBox.appendChild(newItem);
+      resultBox.appendChild(newItemBox);
+    }
+    document.getElementById('Input').value = "";
 }
 
-export default newTask;
+export default Click;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,30 +4,23 @@ import './index.css';
 import {} from '@testing-library/react';
 import NewTask from './components/newTask';
 
-function Click()
-{
-    let inputValue = document.getElementById('Input').value;
-    let resultBox = document.getElementById('mainBox__resultBox');
-    let newItem = document.createElement('input');
-    newItem.setAttribute("value", inputValue);
-    resultBox.appendChild(newItem);
-}
-
 class App extends Component{
   render()
   {
     return (
       <div className="mainBox">
+
         <div className="mainBox__inputBox">
-          <input type="text" id="Input" placeholder="input something"/>
-          <button onClick={Click}>click on me</button>
+          <input type="text" id="Input" placeholder="input something..."/>
+          <input type="button" value="click on me" id="inputButton" onClick={NewTask}/>
         </div>
         
         <div id="mainBox__resultBox">
-          <input value="New task will appear below..."/>
-          {/* <this.Click name={this.state.name}/> */}
-          {/* <NewTask name={this.state.name}/> */}
+          <div className="resultBox__newitemBox">
+            <input className="resultBox__newItem" value="New task will appear below..."/>
+          </div>
         </div>
+
       </div>
     );
   }
