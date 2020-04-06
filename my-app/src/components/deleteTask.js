c53dbb2037bbbf7391a0c6f1ca50440dc30564d6
skrudg <60151747+skrudg@users.noreply.github.com>
2020-04-07 02:33:40 +0600

    Разобрался с запросом на удаление

--- my-app/src/components/deleteTask.js
+++ my-app/src/components/deleteTask.js
@@ -1,15 +1,40 @@
-// import axios from 'axios';
+import axios from 'axios';
+let itemToDelete = "";
+const requestURL = 'http://localhost:3000/task/';
 function deleteTask()
 {
-    // const requestURL = 'http://localhost:3000/task';
-    var close = document.getElementsByClassName("close");
-    var i;
+    let close = document.getElementsByClassName("close");
+    let i;
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
-            var div = this.parentElement;
+            let div = this.parentElement;
+            itemToDelete = div.childNodes[1].value;
             div.style.display = "none";
+
+            axios.get(requestURL)
+                .then(function (response) {
+                    Click(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         };
     }
 }
-
+function Click(props){
+    for(var i = 0; i < props.data.length; i++)
+    {
+        if(props.data[i].taskName === itemToDelete)
+        {
+            // console.log(props.data[i].id);
+            axios.delete(requestURL+props.data[i].id)
+            .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+        }
+    }
+}
 export default deleteTask;
--- my-app/src/components/saveTask.js
+++ my-app/src/components/saveTask.js
@@ -1,14 +1,26 @@
 // import axios from 'axios';
 function saveTask()
 {
-    // var close = document.getElementsByClassName("close");
-    // var i;
-    // for (i = 0; i < close.length; i++) {
-    //     close[i].onclick = function() {
-    //         var div = this.parentElement;
-    //         div.style.display = "none";
-    //     };
-    // }
+    var save = document.getElementsByClassName("save");
+    var i;
+    for (i = 0; i < save.length; i++) {
+        save[i].onclick = function() {
+            var div = this.parentElement;
+            var itemToChange = div.childNodes[1].value;
+            console.log(itemToChange);
+        };
+    }
+
+    // const requestURL = 'http://localhost:3000/task';
+    // axios.put(requestURL)
+    // .then(res => {
+    //   this.setState({
+    //     cat: res
+    //   });
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // })
 }
 
 export default saveTask;
